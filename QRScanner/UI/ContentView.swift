f063ad464365f15a8b34c53753043994035ce95d
Wendy Chen <jaewchen@gmail.com>
2021-03-06 15:12:12 -0600

    Adding style and icons.

--- QRScanner/UI/ContentView.swift
+++ QRScanner/UI/ContentView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message: String? = ""
+    @State private var message: String?
+    @State private var isCapturing: Bool = true
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        QRView()
-        Text(message ?? "")
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                QRView()
+                VStack(spacing: 8) {
+                    Text(message ?? "")
+                        .frame(width: geometry.size.width * 0.25 - 16, height: geometry.size.height - 76)
+                        .border(Color.accentColor.opacity(0.2), width: 4)
+                        .background(Color.accentColor.opacity(0.2))
+                        .cornerRadius(4)
+                    Button(action: { isCapturing = !isCapturing }) {
+                        Image(isCapturing ? "Stop" : "Start")
+                            .renderingMode(.template)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(isCapturing ? .red : .green)
+                            .frame(width: 48, height: 48)
+                            .background(Color.clear)
+                            .border(Color.clear)
+                            .brightness(-0.25)
+                    }
+                }
+                .buttonStyle(BorderlessButtonStyle())
+                .frame(width: geometry.size.width * 0.25, height: geometry.size.height)
+            }
+        }
     }
 }
 
