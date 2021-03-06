c4b25cb8a6fbe4b3308e31cba875f30b4c1a88d0
Wendy Chen <jaewchen@gmail.com>
2021-03-06 15:38:01 -0600

    Small UI changes.

--- QRScanner/UI/ContentView.swift
+++ QRScanner/UI/ContentView.swift
@@ -15,23 +15,28 @@ struct ContentView: View {
         GeometryReader { geometry in
             HStack(spacing: 0) {
                 QRView(message: $message, isCapturing: $isCapturing)
-                VStack(spacing: 8) {
+                VStack(spacing: 16) {
                     Text(message ?? "")
-                        .frame(width: geometry.size.width * 0.25 - 16, height: geometry.size.height - 76)
+                        .frame(width: geometry.size.width * 0.25 - 16, height: geometry.size.height - 96)
                         .border(Color.accentColor.opacity(0.2), width: 4)
                         .background(Color.accentColor.opacity(0.2))
                         .cornerRadius(4)
+                        .font(.title)
                     Button(action: { isCapturing.toggle() }) {
                         Image(isCapturing ? "Stop" : "Start")
                             .renderingMode(.template)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .foregroundColor(isCapturing ? .red : .green)
-                            .frame(width: 48, height: 48)
+                            .frame(width: 24, height: 24)
                             .background(Color.clear)
                             .border(Color.clear)
                             .brightness(-0.25)
                     }
+                    .frame(width: 48, height: 48)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.accentColor, lineWidth: 4))
+                    .shadow(radius: 2)
                 }
                 .buttonStyle(BorderlessButtonStyle())
                 .frame(width: geometry.size.width * 0.25, height: geometry.size.height)
