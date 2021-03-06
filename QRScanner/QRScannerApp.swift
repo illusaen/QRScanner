1875a538512f18e0102d0335abad3e7d5976522e
Wendy Chen <jaewchen@gmail.com>
2021-03-06 13:10:20 -0600

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Wendy Chen on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
